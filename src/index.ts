d7b0defb6ec00e9221b98447951ef59973e7aa71
Juri Adams <juri@adams.sh>
2025-04-21 01:41:37 +0200

    Fixed exports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
-import { optional } from '@/lib/optional';
-import { vars } from '@/lib/vars';
-
 export * from '@/lib/error';
 export * from '@/lib/optional';
 export * from '@/lib/vars';
-
-const env = vars(['OPENAI_API_KEY', optional('PORT')] as const);
-//    ^?
